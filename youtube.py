fe563b909776f74f21a0a16c85631c444712365e
Marllon Fernandes Da Silva <marllon.fernandes@totvs.com.br>
2023-02-07 11:36:27 -0300

    final commit

--- youtube.py
+++ youtube.py
@@ -1,2 +1,2 @@
 from pytube import YouTube
-yt = YouTube("https://www.youtube.com/watch?v=BcC5y22M-hI").streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download(filename="video.mp4")
+yt = YouTube("inserir_url").streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download(filename="video.mp4")
